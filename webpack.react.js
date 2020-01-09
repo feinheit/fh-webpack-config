60d473810424b85529b13911edfb66cdeae0ae4a
Dan Büschlen <dab@feinheit.ch>
2020-01-09 08:35:23 +0100

    use babel react preset

--- webpack.react.js
+++ webpack.react.js
@@ -24,11 +24,11 @@ module.exports = {
                     corejs: "3",
                   },
                 ],
+                ["@babel/preset-react", {}],
               ],
               plugins: [
                 "@babel/plugin-proposal-object-rest-spread",
                 "@babel/plugin-proposal-class-properties",
-                ['@babel/plugin-transform-react-jsx'],
               ],
               cacheDirectory: path.resolve(CWD, "tmp"),
               sourceType: "unambiguous",
